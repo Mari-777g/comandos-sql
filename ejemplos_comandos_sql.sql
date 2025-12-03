bdb6b3a48770e12151c3d2bd1f8717532b9b044c
Mariana Avirama <mariana.avirama07@gmail.com>
2025-12-03 17:27:22 -0500

    Se crea el archivo sql con comandos

--- ejemplos_comandos_sql.sql
+++ ejemplos_comandos_sql.sql
@@ -6,3 +6,41 @@ CREATE DATABASE bdBorrar;
 
 -- DROP DATABASE elimina una base de datos
 DROP DATABASE bdBorrar;
+
+-- USE DATABASE usar una base de datos
+
+USE testDB;
+
+-- CREATE TABLE crea una nueva tabla con atributos
+
+CREATE TABLE Persons(
+per_id int,
+per_last_name varchar(255),
+per_first_name varchar(255),
+per_address varchar(255),
+per_city varchar(255)
+) ;
+
+
+-- DROP TABLE eliminar tabla existente
+
+DROP TABLE Persons;
+
+-- ALERT TABLE agrega, elimina o modifica columnas en la tabla 
+
+-- Agrega columna
+ALTER TABLE Persons
+ADD per_email varchar(255);
+
+-- Elimina columna
+ALTER TABLE Persons
+DROP COLUMN per_email;
+
+-- Modifica columna
+ALTER TABLE Persons
+RENAME COLUMN per_address TO per_phone;
+
+-- Cambia tipo de dato
+
+ALTER TABLE Persons
+MODIFY COLUMN Per_phone varchar(15);
